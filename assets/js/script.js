013afa2a544331d009f56cf26f2fe38010320c1c
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2020-09-09 22:56:59 +0100

    Fixed Establishment and County selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// I am getting two autocomplete options, one from default (Dublin) and the second one (the one which I switch to via county or establishment switcher)
-// Need to figure out why is that? When I get the result from county, it works, however if I select from the second option (the defualt Dublin one)
-// then it gives me an error for children geometry and other shit errors
 const IRELAND_BOUNDS = {
     north: 55.33539361,
     south: 51.4266145,
@@ -15,11 +12,9 @@ var google;
 let popup, Popup;
 var k = 0;
 var input;
-
-// var BOUNDS = {
-//     lat: 1,
-//     lng: 1
-// }
+var searchTypesBar = 'bar'
+var searchTypeLibrary = 'library'
+var searchTypePark = 'park'
 
 var countyList = [{
         "county": "Dublin",
@@ -58,11 +53,7 @@ var countyList = [{
 
 function updateCounty() {
     var select_county_value = document.getElementById("select-county").value;
-    console.log("County value in updateCounty" + select_county_value);
-    var select_establishment = document.getElementById("select-establishment").value;
     if (select_county_value != "") {
-        // var key = select_county_value.replace("value", "");
-        // var current_radius = countyList[0].radius;
 
         for (var i = 0; i < countyList.length; i++) {
             if (countyList[i].county == select_county_value) {
@@ -77,7 +68,6 @@ function updateCounty() {
             infowindowAutoComplete = new google.maps.InfoWindow();
             infowindowContent = document.getElementById('infowindow-content');
             infowindowAutoComplete.setContent(infowindowContent);
-            console.log("OnUpdate: " + infowindowContent);
             onUpdate = true;
         } catch (err) {
             console.log("ERROR: " + err.message);
@@ -90,10 +80,7 @@ function updateCounty() {
 
 function updateEstablishment() {
     var select_establishment = document.getElementById("select-establishment").value;
-    console.log("Establishment value in updateEstablishment" + select_establishment);
     if (select_establishment != "") {
-        // var key = select_county_value.replace("value", "");
-        // var current_radius = countyList[0].radius;
         var select_county_value = document.getElementById("select-county").value;
         for (var i = 0; i < countyList.length; i++) {
             if (countyList[i].county == select_county_value) {
@@ -108,7 +95,6 @@ function updateEstablishment() {
             infowindowAutoComplete = new google.maps.InfoWindow();
             infowindowContent = document.getElementById('infowindow-content');
             infowindowAutoComplete.setContent(infowindowContent);
-            console.log("OnUpdate: " + infowindowContent);
             onUpdate = true;
         } catch (err) {
             console.log("ERROR: " + err.message);
@@ -122,16 +108,13 @@ function updateEstablishment() {
 function initialize(geoLocation, infowindowContent, onUpdate) {
     var card = document.getElementById('pac-card');
     var input = document.getElementById('pac-input');
-    var types = document.getElementById('select-establishment');
-    var strictBounds = document.getElementById('strict-bounds-selector');
 
     if (k == 0) {
         document.getElementById("infowindow-content").style.visibility = "hidden";
     }
 
     var select_county_value = document.getElementById("select-county").value;
-    // var select_establishment = document.getElementById("select-establishment").value;
-    console.log("County value in initialize: " + select_county_value);
+    var select_establishment = document.getElementById("select-establishment").value;
 
     for (var i = 0; i < countyList.length; i++) {
         if (countyList[i].county == select_county_value) {
@@ -140,17 +123,13 @@ function initialize(geoLocation, infowindowContent, onUpdate) {
                 lat: countyList[i].Current_latitude,
                 lng: countyList[i].Current_longitude
             };
-            console.log("Bounds: " + JSON.stringify(BOUNDS));
             break;
         }
     }
 
     if (geoLocation === void 0) {
         var geoLocation = new google.maps.LatLng(countyList[i].Current_latitude, countyList[i].Current_longitude);
-    } else {
-        console.log("geoLocation: " + geoLocation)
     }
-    console.log("County: " + select_county_value)
 
     // Map bit
     map = new google.maps.Map(document.getElementById("map"), {
@@ -163,14 +142,10 @@ function initialize(geoLocation, infowindowContent, onUpdate) {
     var options = {
         bounds: BOUNDS,
         types: ['establishment'],
-        // types: types,
         strictBounds: true
     };
 
-
-    if (onUpdate) {
-        console.log("")
-    } else {
+    if (!onUpdate) {
         infowindowAutoComplete = new google.maps.InfoWindow();
         infowindowContent = document.getElementById('infowindow-content');
         infowindowAutoComplete.setContent(infowindowContent);
@@ -186,29 +161,18 @@ function initialize(geoLocation, infowindowContent, onUpdate) {
     autocomplete.bindTo('bounds', map);
     autocomplete.setFields(['address_components', 'geometry', 'icon', 'name', 'opening_hours', 'place_id', 'rating', 'website', 'formatted_address', 'formatted_phone_number', 'photos', 'price_level', 'reviews', 'user_ratings_total', 'vicinity']);
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        // value = $("#pac-input").val('');
-        // console.log("Value in place_changed: " + value);
-
-        // This piece of code might be the correct one
-        // Test tomorrow
-        // google.maps.event.clearInstanceListeners(autocomplete);
-
 
         infowindowAutoComplete.close();
         marker.setVisible(false);
         var place = autocomplete.getPlace();
         var select_county_value = document.getElementById("select-county").value;
-        console.log("County value in place changed: " + select_county_value);
 
-        if (k === 1) {
-            console.log("result: " + infowindowContent);
-        } else {
+        if (k != 1) {
             result = document.getElementById("infowindow-content");
             result.style.visibility = "visible";
             k++;
         }
 
-        // console.log("Place: " + JSON.stringify(place));
         if (!place.geometry) {
             window.alert("No details available for input: '" + place.name + "'");
             return;
@@ -217,7 +181,6 @@ function initialize(geoLocation, infowindowContent, onUpdate) {
         // If the place has a geometry, then present it on a map.
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
-            console.log("Bounds: " + map.getBounds());
         } else {
             map.setCenter(place.geometry.location);
             map.setZoom(17);
@@ -234,20 +197,11 @@ function initialize(geoLocation, infowindowContent, onUpdate) {
             ].join(' ');
         }
 
-        console.log("Name: " + place.name);
-        // console.log("Address: " + place.address);
-        console.log("Vicinity: " + place.vicinity);
-        // console.log("Phone: " + place.formatted_phone_number);
-        // console.log("Website: " + place.website);
-        // console.log("Rating: " + place.rating);
-        // console.log("Ratings: " + place.user_ratings_total);
-        // console.log("Price: " + place.price_level);
-
         infowindowContent.children['place-icon'].src = place.icon;
         infowindowContent.children['place-name'].textContent = place.name;
         // infowindowContent.children['place-address'].textContent = address;
         // infowindowContent.children['place-vicinity'].textContent = place.vicinity;
-        // infowindowContent.children['place-phone'].textContent = place.formatted_phone_number;
+        infowindowContent.children['place-phone'].textContent = place.formatted_phone_number;
         // infowindowContent.children['place-website'].textContent = place.website;
         // infowindowContent.children['place-rating'].textContent = place.rating;
         // infowindowContent.children['place-user-rating-total'].textContent = place.user_ratings_total;
@@ -255,24 +209,21 @@ function initialize(geoLocation, infowindowContent, onUpdate) {
         infowindowAutoComplete.open(map, marker);
     });
 
-    // map.fitBounds(place.geometry.viewport);
-    console.log("Bounds: " + map.getBounds());
     google.maps.event.trigger(input, 'blur');
     google.maps.event.clearInstanceListeners(input);
 
+    type = [select_establishment]
+
     var request = {
         location: geoLocation,
-        radius: 5000,
-        // types: ["restauraunts"]
-        types: types
+        radius: 10000,
+        types: type
     };
 
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, callback);
 }
-
-
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -290,6 +241,15 @@ function createMarker(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
+        // icon: {
+        //     path: google.maps.SymbolPath.CIRCLE,
+        //     scale: 2,
+        //     opacity: 1,
+        //     fillColor: 'red',
+        //     fillOpacity: 1,
+        //     strokeColor: 'red',
+        //     strokeWeight: 1
+        // },
         position: place.geometry.location
     });
 
